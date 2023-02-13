a8347d33b3f922e814000fb28c5e490e1df625ee
edielam <edguard738@gmail.com>
2023-02-13 17:13:55 +0000

    zombies

--- Savage Rampage/Assets/Scripts/CameraFollow.cs
+++ Savage Rampage/Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,9 @@ public class CameraFollow : MonoBehaviour
 {
     private Transform player;
     private Vector3 tempPOS;
+
+    [SerializeField]
+    private float minX, maxX;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,14 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate() //late update is called when all update computations have been completed
     {
         tempPOS = transform.position; //camera position
-        tempPOS.x = player.position.x; 
+        tempPOS.x = player.position.x;
+
+        if (tempPOS.x < minX) 
+            tempPOS.x = minX;
+        
+        if (tempPOS.x > maxX)
+            tempPOS.x = maxX;
+        
         transform.position = tempPOS; //reassign camera position on x-aixs to player position on x-axis
     }
 }
