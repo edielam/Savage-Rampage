b495c21ec317b5a80ff42c33bce5d27f97f7416a
edielam <edguard738@gmail.com>
2023-02-20 13:00:53 +0000

    ui

---
+++ Savage Rampage/Assets/Scripts/UIController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public void RestartGame()
+    {
+
+    }
+    public void Home()
+    {
+
+    }
+}
