fd04a2b77ed0700840b11904516a55bf74346064
edielam <edguard738@gmail.com>
2023-02-20 12:16:48 +0000

    ui

--- Savage Rampage/Assets/Scripts/MainMenu.cs
+++ Savage Rampage/Assets/Scripts/MainMenu.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
         Debug.Log("Button is pressed");
+        SceneManager.LoadScene("Gameplay");
     }
 }
